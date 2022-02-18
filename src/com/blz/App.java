93e63fc3624597607e69e48800172c32f66ab5b0
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-18 14:05:00 +0530

    first assign

---
+++ src/com/blz/App.java
@@ -0,0 +1,13 @@
+package com.blz;
+
+public class App {
+    public static void main(String args[]){
+        int a=51;
+        int b=52;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
---
+++ src/com/blz/Main.java
@@ -0,0 +1,10 @@
+package com.blz;
+
+public class Main {
+
+    public static void main(String[] args) {
+            System.out.println("Welcome");	// write your code here
+            System.out.println("Welcome");
+            System.out.println("Welcome");
+    }
+}
