7a9dd53736d44bfdd1f44b11183a556d90a897ac
iqbalkhan1997 <98506534+iqbalkhan1997@users.noreply.github.com>
2022-02-18 14:07:28 +0530

    Update Main.java

--- src/com/blz/Main.java
+++ src/com/blz/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
             System.out.println("Welcome");	// write your code here
-            System.out.println("Welcome");
-            System.out.println("Welcome");
+            System.out.println("To");
+            System.out.println("Java");
     }
 }
